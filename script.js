986363eaf95db46e1c1b73a98b20932d491832e9
dereinebre <ilijabertle@outlook.de>
2025-04-07 17:57:43 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Funktion zur Generierung einer zufälligen vierstelligen Zahl
+function generateCombination() {
+  return Math.floor(Math.random() * 9000 + 1000); // 4-stellige Zahl zwischen 1000 und 9999
+}
